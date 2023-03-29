9ed625b4439e2c78a7d83c69c93211670ee7644f
MAHABUB <info.freelancermahabub@gmail.com>
2023-03-29 09:18:54 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,16 +6,22 @@ const Cart = ({cart}) => {
   console.log(cart)
 let totalPrice = 0;
 let totalShipping = 0;
+let quantity = 0;
 for(const product of cart){
-  totalPrice += + product.price;
+  // if(product.quantity === 0){
+  //   product.quantity = 1;
+  // }
+  // product.quantity = product.quantity || 1;
+  totalPrice += + product.price * product.quantity;
   totalShipping += +product.shipping;
+  quantity += + product.quantity
 };
 const tax = (totalPrice * 7 / 100);
 const grandTotal = (totalPrice + totalPrice + tax);
   return (
     <div className='cart'>
         <h4 >Order Summary</h4>
-        <p>Selected Items: {cart.length}</p>
+        <p>Selected Items: {quantity}</p>
         <p>Total Price: ${totalPrice}</p>
         <p>Total Shipping: ${totalShipping}</p>
         <p>Tax: ${tax.toFixed(2)}</p>
